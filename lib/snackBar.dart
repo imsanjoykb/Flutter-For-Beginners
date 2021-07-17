e660939d46a46109357525a3c62e1b78ff743baa
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-17 22:48:35 +0600

    Snackbar file added

---
+++ lib/snackBar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: new ThemeData(
+        primaryColor: const Color(0xFF43a047),
+        accentColor: const Color(0xFFffcc00),
+        primaryColorBrightness: Brightness.dark,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter SnackBar Demo'),
+        ),
+        body: SnackBarPage(),
+      ),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text(
+          'Show SnackBar',
+          style: TextStyle(fontSize: 25.0),
+        ),
+        textColor: Colors.white,
+        color: Colors.redAccent,
+        padding: EdgeInsets.all(8.0),
+        splashColor: Colors.grey,
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Hey! This is a SnackBar message.'),
+            duration: Duration(seconds: 5),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+      ),
+    );
+  }
+}
