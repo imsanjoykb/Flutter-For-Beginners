57fb3d95417b7d6538f503a9946670fc0d1987fd
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-18 23:27:58 +0600

    Progress bar added

---
+++ lib/progressbar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LinearProgressIndicatorApp(),
+    );
+  }
+}
+
+class LinearProgressIndicatorApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LinearProgressIndicatorAppState();
+  }
+}
+
+class LinearProgressIndicatorAppState extends State<LinearProgressIndicatorApp> {
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Linear Progress Bar"),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(12.0),
+          child: _loading
+              ? LinearProgressIndicator()
+              : Text("Press button for downloading", style: TextStyle(fontSize: 25)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _loading = !_loading;
+          });
+        },
+        tooltip: 'Download',
+        child: Icon(Icons.cloud_download),
+      ),
+    );
+  }
+}
